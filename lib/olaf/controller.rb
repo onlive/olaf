586ed2dedca3af2adc2c3454fb845223f22d2a13
Alex Snyatkov <alex.snyatkov@onlive.com>
2013-06-10 10:25:03 -0700

    VAL-160 Fixing lock files

--- lib/olaf/controller.rb
+++ lib/olaf/controller.rb
@@ -193,7 +193,7 @@ module OLFramework
       route = @request.path_info
       route = '/' if route.empty? and not settings.empty_path_info?
       return unless match = pattern.match(route)
-      values += match.captures.to_a.map { |v| force_encoding URI.decode(v) if v }
+      values += match.captures.to_a.map { |v| force_encoding URI.unescape(v) if v }
 
       if values.any?
         original, @params = params, params.merge('splat' => [], 'captures' => values)
