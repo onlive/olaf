e538186d1c9d71b160dbde070ce5cefdcea7cc62
Noah Gibbs <noah@onlive.com>
2013-06-17 14:32:36 -0700

    Add test:integration to all Olaf Rakefiles, currently unused.  VAL-232

--- lib/olaf/tasks/test_task.rb
+++ lib/olaf/tasks/test_task.rb
@@ -1,3 +1,5 @@
+# Copyright (C) 2013 OL2, inc.  All Rights Reserved.
+
 require 'ci/reporter/rake/minitest'
 require 'rake/testtask'
 
@@ -5,7 +7,13 @@ OL_TEST_HELPER = File.join(File.dirname(__FILE__), "..", "test_helpers")
 Rake::TestTask.new do |t|
   t.libs << "test"
   t.ruby_opts.concat ["-r", OL_TEST_HELPER]  # Helper FIRST
-  t.test_files = FileList['test/**/*_test.rb']
+  t.test_files = FileList['test/**/*_test.rb'] - FileList['test/integration/*_test.rb']
   t.verbose = true
 end
 
+Rake::TestTask.new("test:integration") do |t|
+  t.libs << "test"
+  t.ruby_opts.concat ["-r", OL_TEST_HELPER]  # Helper FIRST
+  t.test_files = FileList['test/integration/*_test.rb']
+  t.verbose = true
+end
